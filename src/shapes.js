124c2438d8afd7ca01f550277e122d01a014109c
Julioj1 <dbjulioj@gmail.com>
2025-11-11 19:05:34 -0500

    Completed Shape and Circle

--- src/shapes.js
+++ src/shapes.js
@@ -1,11 +1,22 @@
 /* eslint-disable max-classes-per-file */
 
 class Shape {
-
+  constructor(type) {
+    this.type = type;
+  }
+  getArea() {
+    return 0;
+  }
 }
 
-class Circle {
-
+class Circle extends Shape {
+  constructor(radius) {
+    super("Circle")
+    this.radius = radius;
+  }
+  getArea() {
+    return Math.PI * (this.radius ** 2)
+  }
 }
 
 class Rectangle {
